4861c2c8f1366ce48ce08be6b24967af5d797b90
wandroll <wandrille.verlut@gmail.com>
2014-12-19 09:47:07 +0100

    Update VDMPostsUpdaterTest.php
    Removed example data

--- src/VDM/ScrapperBundle/Tests/Service/VDMPostsUpdaterTest.php
+++ src/VDM/ScrapperBundle/Tests/Service/VDMPostsUpdaterTest.php
@@ -5,12 +5,5 @@ use VDM\ScrapperBundle\Services\VDMPostsUpdater;
 
 class VDMPostsUpdaterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAdd()
-    {
-        $calc = new Calculator();
-        $result = $calc->add(30, 12);
-
-        // vérifie que votre classe a correctement calculé!
-        $this->assertEquals(42, $result);
-    }
+// TODO
 }
