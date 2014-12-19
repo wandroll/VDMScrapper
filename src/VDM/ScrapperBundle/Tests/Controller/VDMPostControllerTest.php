9a8adb6c39adc43679e65b380dd638b738fe58b1
wandroll <wandrille.verlut@gmail.com>
2014-12-19 09:46:24 +0100

    Update VDMPostControllerTest.php
    removed example code

--- src/VDM/ScrapperBundle/Tests/Controller/VDMPostControllerTest.php
+++ src/VDM/ScrapperBundle/Tests/Controller/VDMPostControllerTest.php
@@ -6,12 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class VDMPostControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/api/post/1');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+// TODO
 }
